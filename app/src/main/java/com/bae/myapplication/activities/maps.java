68acf784d5b20be86fc485621fff32d824185da3
elamp <ezgaming3377@gmail.com>
2023-08-26 23:39:50 -0400

    - Added Location Search Feature and Error Handling
    // TODO
    - Add button to request Current Location
    - Add Lat/Lng location to DataStore of each file
    - Retrieve Lat/Lng data from each file when mapping
    - Add Selection feature to plot mapped data by location
    - Add button to transition to maps menu
    - Add button to exit maps menu

--- app/src/main/java/com/bae/myapplication/activities/maps.java
+++ app/src/main/java/com/bae/myapplication/activities/maps.java
@@ -2,10 +2,13 @@ package com.bae.myapplication.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -23,10 +26,14 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
+import java.io.IOException;
+import java.util.List;
+
 public class maps extends AppCompatActivity implements OnMapReadyCallback {
 
     private final int FINE_PERMISSION_CODE = 1;
     private GoogleMap myMap;
+    private SearchView mapsSearchView;
     Location currentLocation;
     FusedLocationProviderClient fusedLocationProviderClient;
 
@@ -37,11 +44,45 @@ public class maps extends AppCompatActivity implements OnMapReadyCallback {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        getLastLocation();
+        // Variable Initialisations
+        mapsSearchView = findViewById(R.id.mapSearchView);
 
 //        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 //        mapFragment.getMapAsync(this);
+
+        mapsSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                String location = mapsSearchView.getQuery().toString();
+
+                if (location != null){
+                    Geocoder geocoder = new Geocoder(maps.this);
+                    try{
+                        List<Address> addressList = geocoder.getFromLocationName(location, 1);
+                        Address address = addressList.get(0);
+                        LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                        myMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                        myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                        return true;
+                    } catch (IOException e){
+                        e.printStackTrace();
+                        Toast.makeText(maps.this, "Unable to process "
+                                + location + "\nPlease try again", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
     }
 
     private void getLastLocation() {
